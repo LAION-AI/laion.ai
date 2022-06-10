6acfd5635a868d5d8e562ffec23ec00d41524bb3
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2022-06-10 14:08:11 -0400

    Subpath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  basePath: "/laion.ai",
+};
+
+module.exports = nextConfig;
