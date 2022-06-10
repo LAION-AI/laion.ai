3fc87269c93ab39b4d3a0d7fc34ad3cf339fa4f7
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2022-06-10 14:18:04 -0400

    BasePath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = {
-  assetPrefix: "/laion.ai",
+  assetPrefix: "/laion.ai/",
   publicRuntimeConfig: {
     basePath: "/laion.ai",
   },
