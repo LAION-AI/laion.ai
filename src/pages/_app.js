5506a7c4cce382f80dc8d83ba5b3688899d9989d
Tomás Carlson <34401946+TomasCarlson@users.noreply.github.com>
2022-06-13 00:34:15 -0400

    Remove cursor-follow

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,16 +3,12 @@ import Image from "next/image";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { useRouter } from "next/router";
 import "../styles/globals.css";
-import Cursor from "react-cursor-follow";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <div className="w-screen full-container flex-col flex">
-      <div className=" z-50">
-        <Cursor color="white" hollow={true} />
-      </div>
       <Nav />
       <div className=" text-white grow flex child:grow flex-col h-full ">
         <div className="h-full w-full relative child:absolute">
