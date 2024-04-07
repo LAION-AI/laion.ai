92499ec8cd98bf1b961cda142450c6fdfea5d7f4
JeniaJitsev <JeniaJitsev@users.noreply.github.com>
2024-04-07 09:03:21 +0000

    deploy: bbdd37cb414f5eb4faac8863ad1321c00f36733c

--- _next/static/O3ENJ-V2sdYOrRbu0rjrg/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fblog-de","\u002Fblog","\u002Fdonations","\u002Fimpressum","\u002Fnotes","\u002Fblog-de\u002F[slug]","\u002Fnotes\u002F[slug]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
