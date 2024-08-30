4d4afb713d12d38da3f623ce519a5f2864281630
robvanvolt <robvanvolt@users.noreply.github.com>
2024-08-30 16:55:04 +0000

    deploy: eb3a27b2bfa1087352e3f4187ad4f4f629776fe2

---
+++ _next/static/nVGku-LUfjEw1wFNazn1m/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout","\u002Fblog-de","\u002Fblog","\u002Fdonations","\u002Fnotes","\u002Fimpressum","\u002Fblog\u002F[slug]","\u002Fnotes\u002F[slug]","\u002Fblog-de\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
