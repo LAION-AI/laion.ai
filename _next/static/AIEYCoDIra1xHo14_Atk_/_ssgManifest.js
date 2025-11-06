9977066c8777fa9b4cfe8992c9837a8eb5eb9c7c
christophschuhmann <christophschuhmann@users.noreply.github.com>
2025-11-06 15:04:01 +0000

    deploy: 4f2b2903e9f631be72cc1e74bda797331579ffe6

---
+++ _next/static/AIEYCoDIra1xHo14_Atk_/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog-de","\u002Fblog","\u002Fdonations","\u002Fabout","\u002Fimpressum","\u002Fnotes","\u002Fblog-de\u002F[slug]","\u002Fnotes\u002F[slug]","\u002Fblog\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
